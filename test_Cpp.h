ad3aba4356e337bf29234100349a2f11eea33a13
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-03-11 13:50:04 +0100

    Update test_Cpp.h
    apprendre creation matrice

--- test_Cpp.h
+++ test_Cpp.h
@@ -8,3 +8,17 @@ m << 1, 2, 3,
      4, 5, 6,
      7, 8, 9;
 std::cout << m;
+
+std::cout << "Matrice carré avec que des zeros:\n";
+Array33f a1 = Array33f::Zero();
+std::cout << a1 << "\n\n";
+ 
+ 
+std::cout << "Vecteur colonne\n";
+ArrayXf a2 = ArrayXf::Zero(3);
+std::cout << a2 << "\n\n";
+ 
+ 
+std::cout << "Matrice 3x4 avec que des zeros:\n";
+ArrayXXf a3 = ArrayXXf::Zero(3, 4);
+std::cout << a3 << "\n";
